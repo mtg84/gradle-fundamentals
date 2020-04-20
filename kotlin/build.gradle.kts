955c3f7d283ad7919be37a679798997c44198f1b
Maria <formacionjee@gmail.com>
2020-04-20 19:10:42 +0200

    Add dependencies

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,6 +1,24 @@
+val log4j_version: String by project
+val jaxb_version: String by project
+val junit_version: String by project
+
 plugins {
-    java
+    application
     id("org.flywaydb.flyway") version "6.3.2"    
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("log4j:log4j:$log4j_version")
+    testImplementation("junit:junit:$junit_version")
 }
 
 version = "1.0-SNAPSHOT"
