0cf8371e65cb81dcf3d3850dafb81d82e26b1b9b
Maria <formacionjee@gmail.com>
2020-04-20 18:24:32 +0200

    Add sourceSets

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -5,6 +5,22 @@ plugins {
 
 version = "1.0-SNAPSHOT"
 
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src"))
+        }
+    }
+    test {
+        setSrcDirs(listOf("test/src"))
+    }
+}
+
+// for plugin application
+/* application {
+    mainClassName = "com.pluralsight.security.Hash"
+} */
+
 tasks.register("hello") {
     doLast {
         print("Hello")
